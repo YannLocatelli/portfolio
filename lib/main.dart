d3f7d82b489775b919cef6db739a9c3ae7d06027
YannLocatelli <yann.locatelli2@gmail.com>
2026-01-23 05:10:09 +0100

    🎨 Move firebase admin syn monitoring

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,12 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // External
 import 'package:portfolio/services/firebase.dart';
-import 'package:portfolio/services/firestore.dart';
-
-// Portfolio
 import 'package:portfolio/supports/router.dart';
 
 void main() async {
   usePathUrlStrategy();
 
   await initializeFirebase();
-  await AdminDatabaseService.syncMonitoring();
 
   runApp(const MyApp());
 }
