a8717f24a899d15aa68b45b15de0645d9df8a215
YannLocatelli <yann.locatelli2@gmail.com>
2026-01-23 20:42:44 +0100

    💄 Set font to Figtree

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // Core
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 // Portfolio
 import 'package:portfolio/services/firebase.dart';
@@ -25,6 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: .fromSeed(seedColor: Colors.orange),
+        textTheme: GoogleFonts.figtreeTextTheme(),
       ),
     );
   }
