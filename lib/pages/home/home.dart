75f39bfbd4459fa2c960a4b822fd6cc8c93bfc14
YannLocatelli <yann.locatelli2@gmail.com>
2026-01-24 20:52:14 +0100

    ✨ Add timeline on HomePage

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -5,8 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 // Portfolio
-import 'package:portfolio/supports/globals.dart' as globals;
-import 'package:portfolio/services/storage.dart';
+import 'package:portfolio/supports/theme.dart';
+import 'package:portfolio/pages/home/timeline.dart';
+import 'package:portfolio/pages/home/timeline_item.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,47 +17,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String? imageURL;
-
-  @override
-  void initState() {
-    super.initState();
-
-    syncStorage();
-  }
-
-  void syncStorage() async {
-    final url = await FirebaseStorageService.getTestFile();
-    setState(() {
-      imageURL = url;
-    });
+  List<Widget> getItems(BuildContext context) {
+    List<Widget> items = [];
+    for (var i = 0; i < 4; i++) {
+      items.add(
+        TimeLineItem(
+          title: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+          description:
+              "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+          year: "20xx",
+          onOpen: () => context.go("/dummy"),
+        ),
+      );
+    }
+    return items;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Yann Locatelli"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            if (imageURL != null)
-              Image.network(
-                imageURL!,
-                fit: BoxFit.cover,
-                webHtmlElementStrategy: WebHtmlElementStrategy.prefer,
-              ),
-            Text('Is online: ${globals.maintenanceEnabled == false}'),
-            TextButton(
-              onPressed: () => context.go("/about"),
-              child: Text("Go About"),
-            ),
-          ],
+        backgroundColor: normalPrimary,
+        title: Text(
+          "Yann Locatelli",
+          style: const TextStyle(color: Colors.white),
         ),
       ),
+      body: Timeline(children: getItems(context)),
     );
   }
 }
